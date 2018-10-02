46862e5514938fae32d1ee1b2a13f94130f63a1d
Krittiya Clark <krittiyaclark@gmail.com>
2018-10-01 23:17:36 -0400

    Styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react';
-import './App.css';
-import GoogleMapDisplay from './components/GoogleMap';
+import React, { Component } from "react";
+import "./App.css";
+import GoogleMapDisplay from "./components/GoogleMap";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <section className="options-panel">
+          <section className="panel-header">
+            <h1>Neighborhood Map</h1>
+            <h4>Eat out in Bentonville, AR!</h4>
+          </section>
+          <section className="panel-body">
+            <section className="search-panel">
+              <input type="text" />
+              <p>Listings provided by FourSquare API</p>
+            </section>
+            <ul class="listings">
+              <li>One</li>
+              <li>Two</li>
+              <li>Three</li>
+            </ul>
+          </section>
+        </section>
         <GoogleMapDisplay isMarkerShown />
       </div>
     );
--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { compose, withProps } from 'recompose';
-import { withScriptjs, withGoogleMap, GoogleMap, Marher } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 
 const GoogleMapDisplay = compose(
-    withProps({
-        googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCpWg0wtatuKxKQ86H2T2W5rtwc_S51XYE",
-        loadingElement: <div style={{ height: `100vh` }} />,
-        containerElelment: <div style={{ height: `400px` }} />,
-        mapElement: <div style={{ height: `100vh` }} />
-    }),
-    withScriptjs,
-    withGoogleMap
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCpWg0wtatuKxKQ86H2T2W5rtwc_S51XYE",
+    loadingElement: <div style={{ height: `100vh` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100vh` }} />
+  }),
+  withScriptjs, 
+  withGoogleMap
 )((props) => {
-    return <GoogleMap
-        defaultZoom={18}
-        defaultCenter={{ lat: 36.372659, lng: -94.208742 }}
-    >
-        {props.isMarkerShown && <Marher position={{ lat: 36.372659, lng: -94.208742 }} />}
-    </GoogleMap>
+  return <GoogleMap
+    defaultZoom={18}
+    defaultCenter={{ lat: 36.372659, lng: -94.208742 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: 36.372659, lng: -94.208742 }} />}
+  </GoogleMap>
 })
-
-export default GoogleMapDisplay;
+ export default GoogleMapDisplay;
