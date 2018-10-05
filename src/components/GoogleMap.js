fe1460e17c94f7a13dcf4943fd982b453b9a76e0
Krittiya Clark <krittiyaclark@gmail.com>
2018-10-04 23:39:14 -0400

    Added ARIA roles and error handling

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,28 +1,49 @@
-import React from "react";
-import { compose, withProps } from "recompose";
+import React, { Component } from "react";
 import { withScriptjs, withGoogleMap, GoogleMap } from "react-google-maps";
 
-// Connects to the Google Maps API
-const GoogleMapDisplay = compose(
-    withProps({
-        googleMapURL : 
-            "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,place" +
-    "s&key=AIzaSyCpWg0wtatuKxKQ86H2T2W5rtwc_S51XYE",
-        loadingElement: <div style={{ height: "100vh", width: "100%" }} />,
-        containerElement: <div style={{ height: "100vh", width: "100%" }} />,
-        mapElement: <div style={{ height: "100vh", width: "100%" }} />
-    }),
-    withScriptjs,
-    withGoogleMap
-)(props => {
-    return (
+const GoogleMapCall = withScriptjs(
+    withGoogleMap(props => (
         <GoogleMap
             defaultZoom={18}
-            defaultCenter={{ lat: 52.370216, lng: 4.895168 }}
+            defaultCenter={{ lat: 36.372659, lng: -94.208742 }}
         >
-            {props.children} />}
+            {props.children}
         </GoogleMap>
-    );
-});
+    ))
+);
+
+class GoogleMapDisplay extends Component {
+    state = {
+        hasError: false
+    };
+
+    componentDidCatch(error, info) {
+        this.setState({
+            hasError: true
+        });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return (
+                <div>
+                    <h1>Something went wrong.</h1>
+                    <h2>You might be offline or the service is down.</h2>
+                    <h3>Check your internet connection and try again.</h3>
+                </div>
+            );
+        }
+        return (
+            <GoogleMapCall
+                googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDWDwhwYWGaZlcFkEIpaLuVY6VjjRNph60"
+                loadingElement={<div style={{ height: "100vh", width: "100%" }} />}
+                containerElement={<div style={{ height: "100vh", width: "100%" }} />}
+                mapElement={<div style={{ height: "100vh", width: "100%" }} />}
+            >
+                {this.props.children}
+            </GoogleMapCall>
+        );
+    }
+}
 
 export default GoogleMapDisplay;
--- src/components/Panel/Listings.js
+++ src/components/Panel/Listings.js
@@ -2,26 +2,39 @@ import React from "react";
 
 // Venue list in the side panel
 const Listings = ({ venues, toggleInfoWindow, searchTerm }) => {
-    return (
-        <ul className="listings">
-            {venues
-                .filter(
-                    venue =>
-                        // Filter the list to only the venue names including the search term
-                        venue.name.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0
-                )
-                .map(venue => {
-                    return (
-                        <li
-                            key={venue.id}
-                            className="listings__item"
-                            onClick={() => toggleInfoWindow(venue)}
-                        >
-                            {venue.name}
-                        </li>
-                    );
-                })}
-        </ul>
-    );
+    if (venues.length === 0) {
+        return (
+            <ul className="listings">
+                <li>No Listings Found</li>
+            </ul>
+        );
+    } else {
+        return (
+            // Asked to add role by reviewer even though ul has an implicit role of list.
+            <ul className="listings" role="list">
+                {venues
+                    .filter(
+                        venue =>
+                            // Filter the list to only the venue names including the search term
+                            venue.name.toLowerCase().indexOf(searchTerm.toLowerCase()) >= 0
+                    )
+                    .map(venue => {
+                        return (
+                            // Asked to add role by reviewer even though li has an implicit role of listitem.
+                            <li
+                                key={venue.id}
+                                tabIndex={0}
+                                role="listitem"
+                                className="listings__item"
+                                onClick={() => toggleInfoWindow(venue)}
+                            >
+                                {venue.name}
+                            </li>
+                        );
+                    })}
+            </ul>
+        );
+    }
 };
+
 export default Listings;
